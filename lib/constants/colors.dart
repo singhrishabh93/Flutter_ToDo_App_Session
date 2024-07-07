569dc17ad37b97a4e666fb77ee8fb9b681f06950
Rishabh Singh <singhrishabh1670@gmail.com>
2024-07-07 16:05:08 +0530

    New Final Release

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-const Color tdRed = Color(0xFFDA4040);
-const Color tdBlue = Color(0xFF5F52EE);
+// const Color tdRed = Color(0xFFDA4040);
+const Color tdRed = Colors.black;
+const Color tdBlue = Color.fromARGB(255, 22, 2, 247);
 
 const Color tdBlack = Color(0xFF3A3A3A);
 const Color tdGrey = Color(0xFF717171);
