18c544028898dd8345f30e11154396be4fae0955
Rishabh Singh <singhrishabh1670@gmail.com>
2024-07-07 15:55:38 +0530

    New Release

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -6,4 +6,5 @@ const Color tdBlue = Color(0xFF5F52EE);
 const Color tdBlack = Color(0xFF3A3A3A);
 const Color tdGrey = Color(0xFF717171);
 
-const Color tdBGColor = Color(0xFFEEEFF5);
+// const Color tdBGColor = Color(0xFFEEEFF5);
+const Color tdBGColor = Colors.yellow;
